cc9e19354df55635d335fc75c728e4566017a03d
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-11-01 00:53:05 -0300

    chore: add UsersFixture

---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,37 @@
+﻿
+
+using CloudCustomers.API.Models;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public class UsersFixture
+    {
+        public static List<User> GetTestUsers() => new()
+        {
+           new User
+            {
+                Id = 1,
+                Name = "Jane",
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Madison",
+                    ZipCode = "53704"
+                },
+                Email = "jane@example.com"
+            },
+           new User
+            {
+                Id = 2,
+                Name = "John",
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Madison",
+                    ZipCode = "53704"
+                },
+                Email = "joe@example.com"
+            },
+        };
+    }
+}
